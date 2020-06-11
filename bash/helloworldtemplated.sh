c496b8dfab907ab29f2527d9f7d939fa10ad23aa
Randeep-2000 <65865653+Randeep-2000@users.noreply.github.com>
2020-06-11 13:01:59 -0400

    Update helloworldtemplated.sh

--- bash/helloworldtemplated.sh
+++ bash/helloworldtemplated.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # My second script - helloworldtemplated.#!/bin/sh
-
+#output to fiels
 cat <<EOF
+# output of script
 Hello World!
+# it gives the procees id
 I am process # $$
+#Multi line input program
 EOF

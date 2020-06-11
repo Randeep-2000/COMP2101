32b191b1f2eca50b010d78b1fbc77efec5f37d49
Randeep-2000 <65865653+Randeep-2000@users.noreply.github.com>
2020-06-11 13:20:27 -0400

    Update helloworldugly.sh

--- bash/helloworldugly.sh
+++ bash/helloworldugly.sh
@@ -4,9 +4,15 @@
 
 # This is a silly way of creating the output text by starting with something else and stream editing it in a pipeline
 echo -n "helb wold" |
+#strem editor
   sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" |
+ # for the translate
   tr "h" "H"|
+  #for the tranlate
   tr "w" "W"|
+  # deffin the txt pattren
   awk '{print $1 "\x20" $2 "\41"}'
+ #basic calculator
 bc <<< "(($$ * 4 - 24)/2 + 12)/2" |
+# strem editor
   sed 's/^/I am process # /'
